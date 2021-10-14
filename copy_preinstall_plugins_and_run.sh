cbae9e912b2e0a4be75f135ef57a5ba1b3323804
larva-zhang <zhanghan0031@163.com>
2021-10-14 23:33:18 +0800

    main:优化Dockerfile

--- copy_preinstall_plugins_and_run.sh
+++ copy_preinstall_plugins_and_run.sh
@@ -6,59 +6,61 @@
 # isHigherVersion 2.0.2332 2.1.0 will return 0.
 # isHigherVersion 2.0.2332 2.0 will return 1.
 function isHigherVersion() {
-    firstVersionArray=(${$1//./})
-    secondVersionArray=(${$2//./})
-    firstVersionArrayLength=${#firstVersionArray[@]}
-    secondVersionArrayLength=${#secondVersionArray[@]}
-    if [ $firstVersionArrayLength -ge $secondVersionArrayLength ]; then
-      for (( i = 0; i < $secondVersionArrayLength; i++ )); do
-          if [ ${firstVersionArray[i]} -gt ${secondVersionArray[i]} ]; then
-              return 1
-          elif [ ${firstVersionArray[i]} -lt ${secondVersionArray[i]} ]; then
-              return 0
-          fi
-      done
-      return 1
-    else
-      for (( i = 0; i < $firstVersionArrayLength; i++ )); do
-          if [ ${firstVersionArray[i]} -gt ${secondVersionArray[i]} ]; then
-              return 1
-          elif [ ${firstVersionArray[i]} -lt ${secondVersionArray[i]} ]; then
-              return 0
-          fi
-      done
-      return 0
-    fi
+  OLD_IFS="$IFS"
+  IFS="."
+  firstVersionArray=($1)
+  secondVersionArray=($2)
+  IFS="$OLD_IFS"
+  firstVersionArrayLength=${#firstVersionArray[@]}
+  secondVersionArrayLength=${#secondVersionArray[@]}
+  if [ $firstVersionArrayLength -ge $secondVersionArrayLength ]; then
+    for ((i = 0; i < $secondVersionArrayLength; i++)); do
+      if [ ${firstVersionArray[i]} -gt ${secondVersionArray[i]} ]; then
+        return 1
+      elif [ ${firstVersionArray[i]} -lt ${secondVersionArray[i]} ]; then
+        return 0
+      fi
+    done
+    return 1
+  else
+    for ((i = 0; i < $firstVersionArrayLength; i++)); do
+      if [ ${firstVersionArray[i]} -gt ${secondVersionArray[i]} ]; then
+        return 1
+      elif [ ${firstVersionArray[i]} -lt ${secondVersionArray[i]} ]; then
+        return 0
+      fi
+    done
+    return 0
+  fi
 }
 
 # copyPlugins $preinstall_plugins_dir $extensions_plugins_dir
 function copyPlugins() {
-    for preinstall_plugin_jar in $(ls $1|grep '.*.jar')
-    do
-      covered_flag=0
-      # sonar-auth-oidc-plugin-2.0.0.jar will return "sonar-auth-oidc-plugin-"
-      preinstall_plugin_name="$(basename $preinstall_plugin_jar "$(echo $preinstall_plugin_jar|grep -Eo '[0-9]+(\.[0-9]+)*.jar')")"
-      for extensions_plugin_jar in $(ls $2|grep '.*.jar') ; do
-          extensions_plugin_name="$(basename $extensions_plugin_jar "$(echo $extensions_plugin_jar|grep -Eo '[0-9]+(\.[0-9]+)*.jar')")"
-          if [ "$preinstall_plugin_name" == "$extensions_plugin_name" ]; then
-              # exists the same name plugin, compare version, the higher version covered the lower version
-              # sonar-auth-oidc-plugin-2.0.0.jar will return "2.0.0"
-              preinstall_plugin_version="$(basename "$(echo $preinstall_plugin_jar|grep -Eo '[0-9]+(\.[0-9]+)*.jar')" .jar)"
-              extensions_plugin_version="$(basename "$(echo $extensions_plugin_jar|grep -Eo '[0-9]+(\.[0-9]+)*.jar')" .jar)"
-              if [ "$(isHigherVersion $preinstall_plugin_version $extensions_plugin_version)" -eq 1 ]; then
-                # do cover
-                rm -rf $2/$extensions_plugin_jar
-                cp $1/$preinstall_plugin_jar $2/
-                covered_flag=1
-              fi
-          fi
-      done
-
-      if [ $covered_flag -eq 0 ]; then
-        # do copy
-        cp $1/$preinstall_plugin_jar $2/
+  for preinstall_plugin_jar in $(ls $1 | grep '.*.jar'); do
+    covered_flag=0
+    # sonar-auth-oidc-plugin-2.0.0.jar will return "sonar-auth-oidc-plugin-"
+    preinstall_plugin_name="$(basename $preinstall_plugin_jar "$(echo $preinstall_plugin_jar | grep -Eo '[0-9]+(\.[0-9]+)*.jar')")"
+    for extensions_plugin_jar in $(ls $2 | grep '.*.jar'); do
+      extensions_plugin_name="$(basename $extensions_plugin_jar "$(echo $extensions_plugin_jar | grep -Eo '[0-9]+(\.[0-9]+)*.jar')")"
+      if [ "$preinstall_plugin_name" == "$extensions_plugin_name" ]; then
+        # exists the same name plugin, compare version, the higher version covered the lower version
+        # sonar-auth-oidc-plugin-2.0.0.jar will return "2.0.0"
+        preinstall_plugin_version="$(basename "$(echo $preinstall_plugin_jar | grep -Eo '[0-9]+(\.[0-9]+)*.jar')" .jar)"
+        extensions_plugin_version="$(basename "$(echo $extensions_plugin_jar | grep -Eo '[0-9]+(\.[0-9]+)*.jar')" .jar)"
+        if [ "$(isHigherVersion $preinstall_plugin_version $extensions_plugin_version)" -eq 1 ]; then
+          # do cover
+          rm -rf $2/$extensions_plugin_jar
+          cp $1/$preinstall_plugin_jar $2/
+          covered_flag=1
+        fi
       fi
     done
+
+    if [ $covered_flag -eq 0 ]; then
+      # do copy
+      cp $1/$preinstall_plugin_jar $2/
+    fi
+  done
 }
 
 set -eux
